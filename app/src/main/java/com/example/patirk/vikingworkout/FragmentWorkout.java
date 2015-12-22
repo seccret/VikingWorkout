ccd993bbfec9a724274f3249a9435c4e055e270b
seccret <olivia.stigeborn@gmail.com>
2015-12-22 20:48:27 +0100

    *Added play button in workout *Added timer and autoscroll in workout

--- app/src/main/java/com/example/patirk/vikingworkout/FragmentWorkout.java
+++ app/src/main/java/com/example/patirk/vikingworkout/FragmentWorkout.java
@@ -3,11 +3,13 @@ package com.example.patirk.vikingworkout;
 import android.graphics.Movie;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -40,21 +42,46 @@ public class FragmentWorkout extends android.support.v4.app.Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.fragment_workout, container, false);
+        final ImageView workoutImage = (ImageView) rootView.findViewById(R.id.ivWorkoutImage);
+        final LinearLayout time = (LinearLayout) rootView.findViewById(R.id.llWorkoutTime);
+        final TextView ivTime = (TextView) rootView.findViewById(R.id.tvWorkoutTime);
         final ImageView gif = (ImageView) rootView.findViewById(R.id.ivWorkoutImage);
-        TextView tips = (TextView) rootView.findViewById(R.id.tvWorkoutTips);
-        ListView lvExercises = (ListView) rootView.findViewById(R.id.lvWorkoutList);
-        Workout workout = MainActivity.currentWorkout;
+        final TextView tips = (TextView) rootView.findViewById(R.id.tvWorkoutTips);
+        final ImageView play = (ImageView) rootView.findViewById(R.id.ivWorkoutPlay);
+        final ListView lvExercises = (ListView) rootView.findViewById(R.id.lvWorkoutList);
+        final Workout workout = MainActivity.currentWorkout;
         final List<Integer> exercises = workout.getExercises();
 
         if (workout.getPicture() == 1) {
-            gif.setBackgroundResource(R.drawable.workout1);
+            workoutImage.setBackgroundResource(R.drawable.workout1);
         } else if (workout.getPicture() == 2) {
-            gif.setBackgroundResource(R.drawable.workout2);
+            workoutImage.setBackgroundResource(R.drawable.workout2);
         } else if (workout.getPicture() == 3) {
-            gif.setBackgroundResource(R.drawable.workout3);
+            workoutImage.setBackgroundResource(R.drawable.workout3);
         }
 
         tips.setText("This is an " + workout.getName() + " workout!");
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                time.setVisibility(View.VISIBLE);
+                play.setVisibility(View.INVISIBLE);
+                final int currentExercise = 1;
+
+                new CountDownTimer(3000, 1000) {
+
+                    public void onTick(long millisUntilFinished) {
+                        ivTime.setText(""+ millisUntilFinished / 1000);
+                    }
+
+                    public void onFinish() {
+                        ivTime.setText("done!");
+                        lvExercises.setSelection(currentExercise);
+                    }
+                }.start();
+            }
+        });
 
         AdapterImage2 ai = new AdapterImage2(MainActivity.mainActivity, exercises);
         lvExercises.setAdapter(ai);
